0654fd898af303a7dd5f3ce41802d220e438aa13
Frederick Hornsey <fred@hornsey.us>
2017-02-20 16:10:36 -0600

    Added docstrings to tag/models.py

--- tag/models.py
+++ tag/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Abstract_Tag(models.Model):
+    '''Abstract Base class for Tags'''
     name = models.CharField(
         primary_key = True,
         max_length = 256,
@@ -13,18 +14,28 @@ class Abstract_Tag(models.Model):
         return self.name
 
 class Tag(Abstract_Tag):
+    '''Concrete Main Tag Class'''
     pass
 
 class Meta_Tag(Abstract_Tag):
+    '''Tag that defines the name of relationship between a Tagged_Object
+    and a Tag.
+    '''
     pass
 
 class Tagged_Object(models.Model):
+    '''Base class for Objects that are tagged'''
+
     def tag(self, *tags):
+        '''Tag an object with a Tag instance or a string. Will create tag
+        if string doesn't exist as a tag.
+        '''
+
         tag_instance = None
         for tag in tags:
             if isinstance(tag, Tag):
                 if self.tagging_set.filter(tag__name = tag.name):
-                    continue
+                    continue # Already tagged with this tag
                 else:
                     tag_instance = tag
             else:
@@ -38,6 +49,11 @@ class Tagged_Object(models.Model):
             Tagging(tag = tag_instance, object = self).save()
 
     def untag(self, *tags):
+        '''Takes the same format of arguments as tag(), except it untages
+        it from the object (Deletes Taggings). Ignores tags that don't exist
+        and tags that are not tagged on this object.
+        '''
+
         name = None
         for tag in tags:
             if isinstance(tag, Tag):
@@ -48,15 +64,21 @@ class Tagged_Object(models.Model):
             self.tagging_set.filter(tag__name = name).delete()
             
     def tags(self):
+        '''Get Tag instances that this object is tagged with'''
+
         return [i.tag for i in self.tagging_set.all()]
 
     def tag_strs(self):
+        '''Get Tag names as strings that this object is tagged with'''
+
         return [i.name for i in self.tags()]
 
     def __str__(self):
         return str(self.tag_strs())
 
 class Tagging(models.Model):
+    '''Relationship between a Tagged_Object and a Tag.'''
+    
     tag = models.ForeignKey(
         Tag,
         on_delete = models.CASCADE,
@@ -74,6 +96,8 @@ class Tagging(models.Model):
         )
 
 class Meta_Tagging(models.Model):
+    '''Relationship between a Tagged_Object, Meta_Tag and Tag.'''
+
     meta_tag = models.ForeignKey(
         Meta_Tag,
         on_delete = models.CASCADE,
