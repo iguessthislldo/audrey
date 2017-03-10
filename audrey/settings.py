6a0bfcca6ba19057951ac89e4ec27643834ddaed
Frederick Hornsey <fred@hornsey.us>
2017-03-10 13:32:03 -0600

    Renamed Project from "dothing" to "audrey"

--- audrey/settings.py
+++ audrey/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'dothing.urls'
+ROOT_URLCONF = 'audrey.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'dothing.wsgi.application'
+WSGI_APPLICATION = 'audrey.wsgi.application'
 
 
 # Database
